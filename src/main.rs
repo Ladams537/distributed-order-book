4118c998e2abb37fea9167137bb05a834fc63b94
ladams537 <louistadams3@hotmail.com>
2025-11-26 16:51:29 +0000

    scooter-basics
    Development of primitive data structures and add_order implementation
    for order_book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use slab::Slab;
+use std::collections::{BTreeMap, VecDeque};
+
+// Simple scooter
+#[derive(Debug, Clone, Copy)]
+struct Order {
+    id: u64,
+    price: u64,
+    quantity: u64,
+}
+
+// Pointer: strong typing for index
+type OrderIndex = usize;
+
+// Queue of pointers
+// Stays hot in L1 cache
+#[derive(Debug)]
+struct Level {
+    orders: VecDeque<OrderIndex>,
+}
+
+struct OrderBook {
+    // Contiguous memory block
+    // O(1) access by index
+    arena: Slab<Order>,
+
+    // Maps price -> level
+    // Sorted by price
+    bids: BTreeMap<u64, Level>,
+    asks: BTreeMap<u64, Level>,
+}
+
+impl OrderBook {
+    fn new() -> OrderBook {
+        Self {
+            arena: Slab::new(),
+            bids: BTreeMap::new(),
+            asks: BTreeMap::new(),
+        }
+    }
+
+    fn add_order(&mut self, price: u64, quantity: u64) {
+        // 1. Allocate in Arena (Data)
+        let entry = self.arena.vacant_entry();
+        let id = entry.key();
+        entry.insert(Order {
+            id: id as u64,
+            price: price,
+            quantity: quantity,
+        });
+
+        // 2. Insert Index into Level (Ordering)
+        self.bids
+            .entry(price)
+            .or_insert_with(|| Level {
+                orders: VecDeque::new(),
+            })
+            .orders
+            .push_back(id);
+    }
+}
+
 fn main() {
     println!("Hello world");
 }
